7fa6b1f379a93046be8b56c4f4c4c07b6f45cbf1
Siyabonga Gregory <huge.fuze@gmail.com>
2022-01-04 10:24:40 +0200

    Basic weather card styling

--- src/App.js
+++ src/App.js
@@ -5,37 +5,37 @@
  * Developer's E-mail   : huge.fuze@gmail.com
  * */
 
-import React, { useRef,useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import MapView from "@arcgis/core/views/MapView";
-import Map from "@arcgis/core/Map";
 import Search from "@arcgis/core/widgets/Search";
+import WeatherCard from "./compnents/WeatherCard";
+import Map from "@arcgis/core/Map";
 import axios from "axios";
-import "./App.css"; 
-import WeatherCard from "./compnents/WeatherCard";  
+import "./App.css";
 
 function App() {
-  const[data,setData]=useState([]);  //Weather Data
-  const ARCGIS_API_KEY='AAPKbfc7bf7f40df4b0994b01f630584f041JUySrYc4buTUVL5OKuzWOfWcZHpcq4qJW753W9Xr6QfvGme74dEYmNTfYpnFVaO0';// ARCGIS_API_KEY
-  const OPEN_WEATHER_API_KEY='aa5a2db388766f55fc4fd88ca213511a';// OPEN_WEATHER_API_KEY
-   
+  const [data, setData] = useState([]);  //Weather Data
+  const ARCGIS_API_KEY = 'AAPKbfc7bf7f40df4b0994b01f630584f041JUySrYc4buTUVL5OKuzWOfWcZHpcq4qJW753W9Xr6QfvGme74dEYmNTfYpnFVaO0';// ARCGIS_API_KEY
+  const OPEN_WEATHER_API_KEY = 'aa5a2db388766f55fc4fd88ca213511a';// OPEN_WEATHER_API_KEY
+
   const mapDiv = useRef(null);
-    const PingAddress = (address) => {
-      const request = axios.get(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=${address}&+category=&outFields=*&forStorage=false&f=pjson`)
+  const PingAddress = (address) => {
+    const request = axios.get(`https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?SingleLine=${address}&+category=&outFields=*&forStorage=false&f=pjson`)
       .then((response) => {
         fetchWeatherData(response.data['candidates'][0].location);
       })
-    .catch(console.log)
-}
+      .catch(console.log)
+  }
 
-const fetchWeatherData=async(AddressData)=>{
-  console.log(AddressData);
-  const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${AddressData.y}&lon=${AddressData.x}&appid=${OPEN_WEATHER_API_KEY}`)
+  const fetchWeatherData = async (AddressData) => {
+    console.log(AddressData);
+    const request = axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${AddressData.y}&lon=${AddressData.x}&appid=${OPEN_WEATHER_API_KEY}`)
       .then((response) => {
         setData(response)
         console.log(response)
       })
-    .catch(console.log)
-}
+      .catch(console.log)
+  }
 
   useEffect(() => {
     if (mapDiv.current) {
@@ -46,16 +46,16 @@ const fetchWeatherData=async(AddressData)=>{
       const view = new MapView({
         container: mapDiv.current,
         map: map,
-        center: [-29.85,38],
+        center: [-29.85, 38],
         scale: 10000000
       });
       const searchWidget = new Search({
         view: view,
-        popupEnabled:true,
+        popupEnabled: true,
         allPlaceholder: "Please type address here",
         includeDefaultSources: false,
         sources: [
-          {   
+          {
             name: "Siyabonga Gregory",
             placeholder: "Please type address here",
             apiKey: ARCGIS_API_KEY,
@@ -63,22 +63,24 @@ const fetchWeatherData=async(AddressData)=>{
             locator: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer"
           }
         ]
-      }); 
+      });
 
-      searchWidget.on("select-result", function(event){
+      searchWidget.on("select-result", function (event) {
         PingAddress(event.result.name);
       });
 
       view.ui.add(searchWidget, {
-        position: "top-right"
+        position: "top-left"
       });
       view.ui.remove("zoom");
     }
   }, []);
 
-  return <>
-           <div className="mapDiv" ref={mapDiv} style={{padding:'0px 10px', position: 'fixed'}}></div>;
-           </>
+  return (<>
+    <div className="mapDiv" ref={mapDiv}></div>;
+    <div className="weatherCard"><WeatherCard weatherData={data} /></div>
+  </>)
+
 }
 
 export default App;
--- src/compnents/WeatherCard.jsx
+++ src/compnents/WeatherCard.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
+import { textAlign } from '@mui/system';
+import React, { createElement } from 'react'
 
-export default function WeatherCard({weatherData}) {
-    console.log('Weather Data    '+JSON.stringify(weatherData));
-    const refresh=()=>{
-        window.location.reload();
-    }
+export default function WeatherCard({ weatherData }) {
+    console.log('Weather Data    ' + JSON.stringify(weatherData));
+
+    /* var temp=(weatherData.main.temp-273.15).toFixed(2);
+    var minTemp=(weatherData.main.temp_min -273.15).toFixed(2);
+    var maxTemp=(weatherData.main.temp_max- 273.15).toFixed(2);
+
+    console.log(temp + '    ' + minTemp + '   ' + maxTemp);
+
+    var currentDate = new Date().getDate();
+    var year = new Date().getFullYear();
+    var month=new Date().toLocaleString('default',{month:'long'});
+    
+    console.log(currentDate + '     ' + year + '    ' + month); */
     return (
         <>
-           <div className='container'>
-               <div className='row'>
-                   <div className='col-md-4'>
-                       <div className='card text-wtite gb-primary mb-3'>
-                           <div className='card-header'>Weather
-                           <i className='fa fa-refresh float-end pt-1' onClick={refresh}></i>
-                           </div>
-                           <div className='card-body'>
-                               <h5 className='card-title'>Primary card title</h5>
-                               <p className='card-text'>Some This Awesome IS Coming Soon</p>
-                           </div>
-                       </div>
-                   </div>
-               </div>
-           </div>
+            <div className="card text-white bg-primary mb-3"  
+            style={{maxWidth: 350+'px' , borderRadius:5+'px' , textAlign:'left' , paddingLeft:9+'px' ,
+            fontFamily:"Times New Roman, Times, serif"}}>
+                <div className="card-header" style={{ fontSize:24 +'px'}}>H</div>
+                <div className="card-body">
+                    <h5 className="card-title">PT</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
         </>
     )
 }
